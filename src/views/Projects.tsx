751713367c1035b91b5165f54ce09cc6b7e2414c
Kisura W.S.P <kisura2003@gmail.com>
2024-07-26 21:31:55 +0530

    currently working on this page

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,10 +1,18 @@
+import Card from "react-bootstrap/Card";
 import KNavBar from "../components/Nav.component";
 
 const Projects : React.FC = () => {
     return(
         <>
             <KNavBar/>
-            
+            <Card style={{ width: '18rem', margin: "1rem" }}>
+                <Card.Body>
+                    <Card.Title><a href="https://github.com/KisuraWSP/KisuraWSP.github.io">Portfolio</a></Card.Title>
+                    <Card.Text>
+                        This is my portfolio site written using Vite + TS + React 
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </>
     );
 };
