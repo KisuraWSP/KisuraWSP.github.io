ffc0cdea0c3c4668a2380316b85fd73ac0661647
Kisura W.S.P <kisura2003@gmail.com>
2024-08-09 22:27:45 +0530

    updated skills page now it will look better

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import "./assets/css/index.css";
+import 'devicon/devicon.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
