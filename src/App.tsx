cfe2b7d6438513fea1cf13444e6b73897acb4869
KisuraWSP <kisura2003@gmail.com>
2025-07-01 13:22:05 +0530

    making modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Explore from './views/ExplorePage.tsx';
 import Home from './views/HomePage.tsx';
+import Experience from './views/ExperiencePage.tsx';
 
 function App() {
   return(
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/explore" element={<Explore/>}/>
+        <Route path='/experience' element={<Experience/>}/>
       </Routes>
     </>
   );
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -34,7 +34,7 @@ function Home() {
     const { scrollYProgress } = useScroll();
 
     const profile_logo: CSSProperties = {
-        width: isMobile ? "16rem" : "22rem",
+        width: isMobile ? "18rem" : "26rem",
         height: isMobile ? "18rem" : "26rem",
         marginBottom: isMobile ? "1rem" : "0",
     };
@@ -86,7 +86,7 @@ function Home() {
             <Container style={containerStyle}>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} className="d-flex flex-column align-items-center">
-                        <Image src={profile} rounded style={profile_logo} loading="lazy" />
+                        <Image src={profile} roundedCircle style={profile_logo} loading="lazy" />
                         <h1 style={headerStyle}>Hello</h1>
                         <h2 style={{ color: "#8642F6", textAlign: isMobile ? "center" : "left", 
                             fontWeight:"bold"
