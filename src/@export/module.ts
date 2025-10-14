b87f4ede1c9794b4678256f54fd83c576ae0f86b
KisuraWSP <kisura2003@gmail.com>
2025-10-14 15:23:02 +0530

    added extra links

--- src/@export/module.ts
+++ src/@export/module.ts
@@ -9,3 +9,11 @@ export const github = () => {
 export const itch_io = () => {
     window.location.href = "https://redstonemc2003.itch.io";
 };
+
+export const youtube = () => {
+    window.location.href = "https://www.youtube.com/@redstonemc2003";
+};
+
+export const twitter = () => {
+    window.location.href = "https://x.com/redstonemc2003";
+};
